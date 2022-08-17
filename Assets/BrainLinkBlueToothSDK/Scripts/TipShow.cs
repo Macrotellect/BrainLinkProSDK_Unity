990468b909f495528e64aee79272030e164b2025
1798365869@qq.com <1798365869@qq.com>
2022-08-17 14:07:06 +0800

    upload V1.0.4

---
+++ Assets/BrainLinkBlueToothSDK/Scripts/TipShow.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+public class TipShow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject obj_textTip;
+    void Start()
+    {
+#if UNITY_ANDROID
+        obj_textTip.SetActive(true);
+ #else
+        obj_textTip.SetActive(false);
+#endif
+        
+    }
+
+    
+}
